23ce35ac893490a63cbcf0e3318fee9118596e4b
Bryce <brycedeso123@gmail.com>
2021-01-11 17:03:42 -0600

    Added to player branch

--- raygame/Player.h
+++ raygame/Player.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "Actor.h"
+
+class Player : public Actor
+{
+public:
+	Player();
+	Player(float health);
+
+	bool getIsAlive();
+	float getHealth() { return m_health; }
+
+private:
+	float m_health;
+};
